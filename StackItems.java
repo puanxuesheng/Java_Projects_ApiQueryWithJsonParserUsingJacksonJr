acf18473f5ff78cc439e0893acda6a891cde9a5d
puanxuesheng <57169314+puanxuesheng@users.noreply.github.com>
2020-04-01 15:25:14 +0800

    Update StackItems.java
    Added json ignore properties to ignore "unknown properties" of the JSON

--- StackItems.java
+++ StackItems.java
@@ -2,6 +2,8 @@ package com.example.webjson;
 
 import java.util.List;
 
+//use the json ignore properties and set unknown to be true. Only apply to Jackson not Jackson jr
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class StackItems {
     private List<QueryResultBean> items;
 
